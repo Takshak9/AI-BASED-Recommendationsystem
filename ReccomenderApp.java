1c803cab6b656fa5bb2073a74df2d33d2e1cc5ba
Takshak9 <165394861+Takshak9@users.noreply.github.com>
2025-05-28 20:29:02 +0530

    Create ReccomenderApp.java

---
+++ ReccomenderApp.java
@@ -0,0 +1,35 @@
+	package com.codtech.recommender;
+	
+	import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+	import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+	import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+	import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+	import org.apache.mahout.cf.taste.model.DataModel;
+	import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+	import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+	import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+	
+	import java.io.File;
+	import java.util.List;
+	
+	public class RecommenderApp {
+	    public static void main(String[] args) {
+	        try {
+	            DataModel model = new FileDataModel(new File("src/main/resources/data/ratings.csv"));
+	
+	            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+	            UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+	
+	            GenericUserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+	
+	            List<RecommendedItem> recommendations = recommender.recommend(1, 3);  // for user 1, recommend top 3 items
+	
+	            for (RecommendedItem recommendation : recommendations) {
+	                System.out.println("Recommended Item: " + recommendation.getItemID() +
+	                                   " Value: " + recommendation.getValue());
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
